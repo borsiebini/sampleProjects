b2906e66455446dd80d0ca80efeb0d3e08de8b1c
borsiebini <boris.ebini@gmail.com>
2016-04-15 14:29:36 -0400

    Include mock testCases

---
+++ java.git.maven.sample/src/test/java/com/java/git/maven/sample/connect/test/GetGitConnectionTest.java
@@ -0,0 +1,15 @@
+package com.java.git.maven.sample.connect.test;
+
+import junit.framework.TestCase;
+
+public class GetGitConnectionTest extends TestCase {
+
+	public void testEstablishConnection() {
+		assertTrue(true);
+	}
+
+	public void testEstablishConnectionString() {
+		assertTrue(true);
+	}
+
+}
---
+++ java.git.maven.sample/src/test/java/com/java/git/maven/sample/request/test/RequestFromRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.java.git.maven.sample.request.test;
+
+import junit.framework.TestCase;
+
+public class RequestFromRepositoryTest extends TestCase {
+
+	public void testGetAllBranches() {
+		assertTrue(true);
+	}
+
+	public void testGetAllTags() {
+		assertTrue(true);
+	}
+
+	public void testFindObjectWith() {
+		assertTrue(true);
+	}
+
+}
